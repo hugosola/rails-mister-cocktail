910b66f8fa5cb08b5785f22b46fc5a099518f97f
hugo sola <hugo.degeorges@gmail.com>
2018-05-18 01:21:37 +0200

    fleme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+console.log("Hello from app/javascript/packs/application.js!");
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,6 +8,9 @@ class DosesController < ApplicationController
 
   def create
     @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @dose.save
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
