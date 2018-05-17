2068dcbe27c1363ce7756ce0ba4bfd47f4c0acf0
hugo sola <hugo.degeorges@gmail.com>
2018-05-18 00:30:12 +0200

    addjs

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,12 +1,39 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :destroy]
+
   def index
     @cocktails = Cocktail.all
   end
+
   def show
-    @cocktails = Cocktail.find(params[:id])
+    @cocktail = Cocktail.find(params[:id])
   end
   def new
-    @cocktails = Cocktail.new
+    @cocktail = Cocktail.new
   end
 
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+    private
+
+    def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+    end
+
+    def cocktail_params
+    params.require(:cocktail).permit(:name, :doses, :pictureurl)
+    end
 end
